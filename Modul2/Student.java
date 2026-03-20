b581fc8ee037f5b1d8bd9f5eed95196ab18a1176
Baiq Aqila Naura Rayyani <aqila22baiq@gmail.com>
2026-03-20 20:29:07 +0800

    Create Student.java

---
+++ Modul2/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    public String nama;
+    public int skor;
+
+    public Student(String nama) {
+        this.nama = nama;
+    }
+
+    void setNama(String nama) {
+        this.nama = nama;
+}
+public int getSkor() {
+return this.skor;
+    }
+}
