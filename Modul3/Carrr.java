6a3025f00a4e23e12d090144efd093b11585cfad
Baiq Aqila Naura Rayyani <aqila22baiq@gmail.com>
2026-03-20 20:23:37 +0800

    Create Carrr.java

---
+++ Modul3/Carrr.java
@@ -0,0 +1,12 @@
+public class Carrr {
+    public int numWheel = 4;
+    Car() {
+        System.out.println("Car is created");
+    }
+    public void drift() {
+        System.out.println("Car is drifting");
+    }
+    public void honk() {
+        System.out.println("Car is honking");
+    }
+}
