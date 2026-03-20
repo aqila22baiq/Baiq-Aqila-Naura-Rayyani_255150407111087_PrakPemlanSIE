1d0bea5b01d01a2b9d94c5bedce09f0d408d50e2
Baiq Aqila Naura Rayyani <aqila22baiq@gmail.com>
2026-03-20 19:30:25 +0700

    Add files via upload

---
+++ Modul2/Lingkaran.java
@@ -0,0 +1,11 @@
+package staticandnonstatic;
+public class Lingkaran {
+    public static float PI = 3.14f;
+    public float r;
+    public Lingkaran(float r) {
+        this.r = r;
+    }
+    public float luas() {
+        return Lingkaran.PI * r * r;
+    }
+}
